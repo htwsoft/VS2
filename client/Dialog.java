4033f3110bd22f23556a2ebb84b0167841386380
kasakika <salvatore261@web.de>
2017-02-01 08:35:13 +0100

    Add files via upload
    ueberarbeitet

--- client/Dialog.java
+++ client/Dialog.java
@@ -7,40 +7,41 @@ public class Dialog {
 
 	public static void main(String args[]) {
 		ArrayList<MessageData> messageListtest = new ArrayList<MessageData>();
-		
+
 		boolean shutdown = false;
 		String messagea;
-        String messageID;
+		String messageID;
 		String bName;
-        MessageData msgData;
+		MessageData msgData;
 		int loeschuid;
 		StartClient test;
-		//StartClient test = new StartClient("127.0.0.1",6000);
-		test = new StartClient(args);
+		 test = new StartClient("127.0.0.1",6000);
+//		test = new StartClient(args);
+		 System.out.println("Benutzer name:");
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Username:");
 		String username = scan.nextLine();
-		
-		test.writeUser(username);
+
+		test.loginBenutzer(username, "12345");
 		int i;
-		boolean beenden=false;
-		
+		boolean beenden = false;
+
 		while (!beenden) {
-			System.out.println("\n Was möchten Sie machen?"
-					+ "\nBeenden:0 " 
-					+ "\nNachricht schreiben :1" 
-						+ "\nNachricht löschen 2:"
-					+ "\nNachricht ersetzen: 3" 
-						+ "\nNachricht Ausgabe: 4"
-						+ "\nServer wechseln 5 (automatisch): " + "\n -----> ");
+			System.out.println("\n Was möchten Sie machen?" +
+								"\nBeenden:0 " + 
+								"\nNachricht schreiben :1"
+								+ "\nNachricht löschen 2:" 
+								+ "\nNachricht ersetzen: 3" 
+								+ "\nNachricht Ausgabe: 4"
+								+ "\nServer wechseln 5 (automatisch): "
+								+ "\nGebe Vater und kinder Infos 6" + "\n -----> ");
 			i = scan.nextInt();
 			switch (i) {
 			case 1:
-				
+
 				System.out.println("Message:");
 				Scanner scan1 = new Scanner(System.in);
-				 messagea = scan1.nextLine();
-				 
+				messagea = scan1.nextLine();
+
 				test.schreibeMessage(messagea);
 				break;
 
@@ -50,65 +51,75 @@ public class Dialog {
 				loeschuid = scan.nextInt();
 				System.out.println("ID:");
 				scan = new Scanner(System.in);
-               			messageID = scan.nextLine();
-                
-				if(!test.deleteMessage(loeschuid, messageID)){
-					System.out.println("UID: "+loeschuid+" und/oder "
-							+ "ID: "+messageID+ " sind falsch ");
+				messageID = scan.nextLine();
+
+				if (!test.deleteMessage(loeschuid, messageID)) {
+					System.out.println("UID: " + loeschuid + " und/oder " + "ID: " + messageID + " sind falsch ");
 				}
 				break;
 
 			case 3:
-                String [] childs =  test.getChildNames();
-			    System.out.println(childs[0]);
-//				System.out.println("Ersetzen : \n ");
-//				System.out.println("Message : ");
-//				Scanner scan11 = new Scanner(System.in);
-//				messagea = scan11.nextLine();
-//				System.out.println("UserName : \n ");
-//				scan = new Scanner(System.in);
-//				bName = scan.nextLine();
-//				System.out.println("UID : \n ");
-//				scan = new Scanner(System.in);
-//				loeschuid = scan.nextInt();
-				
-//				if(!test.setMessage(messagea, loeschuid, bName)){
-//					System.out.println("UID: "+loeschuid+" und/oder "
-//							+ "Massage: "+messagea
-//							+ " Bname: bName"
-//							+ " sind falsch ");
-//				}
+
+				System.out.println("Ersetzen : \n ");
+				System.out.println("neue Message : ");
+				Scanner scan11 = new Scanner(System.in);
+				messagea = scan11.nextLine();
+				System.out.println("UserName : \n ");
+				scan = new Scanner(System.in);
+				bName = scan.nextLine();
+				System.out.println("UID : \n ");
+				scan = new Scanner(System.in);
+				loeschuid = scan.nextInt();
+				System.out.println("MessageID : \n ");
+				scan = new Scanner(System.in);
+				String mId = scan.nextLine();
+
+				if (!test.setMessage(messagea, mId ,loeschuid, bName)) {
+					System.out.println("UID: " + loeschuid + " und/oder " + "Massage: " + messagea + " Bname: bName"
+							+ " sind falsch ");
+				}
 				break;
 			case 4:
-				
+
 				System.out.println("\n Ausgabe von alle Message auf server:\n");
-				messageListtest=test.getMessage();
-				for(int y=0;y<messageListtest.size();y++){
-					System.out.println("TEXT:"+messageListtest.get(y).text+
-							" -BNAME:"+messageListtest.get(y).uName+
-							" -UID:"+messageListtest.get(y).uid+
-							" -ID:"+messageListtest.get(y).id);
+				messageListtest = test.getMessage();
+				for (int y = 0; y < messageListtest.size(); y++) {
+					System.out.println("TEXT:" + messageListtest.get(y).text + " -BNAME:" + messageListtest.get(y).uName
+							+ " -UID:" + messageListtest.get(y).uid + " -ID:" + messageListtest.get(y).id);
 				}
 				break;
 			case 5:
 				System.out.println("Bitte ORBD und Server neustarten mit der neue ip wenn gleich PC");
-				String ip=test.getFatherIP();
-				int port=test.getFatherPort();
+				String ip = test.getFatherIP();
+				int port = test.getFatherPort();
 				test.disconnectToServer();
-				System.out.println(ip+port);
+				System.out.println(ip + port);
 				System.out.println("Moechten sie jetzt verbindne (y eingbene)");
 				scan = new Scanner(System.in);
-				String eingabe=scan.nextLine();
-				
-				if(eingabe.equals("y")){
-				System.out.println(ip+port);
-				test = new StartClient(ip,port);
-				test.writeUser(username);
+				String eingabe = scan.nextLine();
+
+				if (eingabe.equals("y")) {
+					System.out.println(ip + port);
+					test.loginBenutzer(username, "12345");
+					test = new StartClient(ip, port);
+
 				}
+				break;
+			case 6:
+				String childname=test.getChildNames().get(0);
+				System.out.println("Kindname: "+childname+" IP: "+test.getChildIP(childname)+" Port"+test.getChildPort(childname));
+				
+				childname=test.getChildNames().get(1);
+				System.out.println("Kindname: "+childname+" IP: "+test.getChildIP(childname)+" Port"+test.getChildPort(childname));
+				
+				
+				System.out.println(test.getFatherName());
+				System.out.println(test.getFatherIP());
+				System.out.println(test.getFatherPort());
 				break;
 			case 0:
 				System.out.println("Beendet");
-				beenden=true;
+				beenden = true;
 				break;
 			default:
 				System.out.println("bitte gib eine richtige Eingabe!\n");
--- client/StartClient.java
+++ client/StartClient.java
@@ -1,3 +1,4 @@
+
 /**
  *
  * @author imed
@@ -5,133 +6,183 @@
 
 import java.util.ArrayList;
 import org.omg.CosNaming.*;
-import org.omg.CosNaming.NamingContextPackage.*;
-
-import java.util.Scanner;
-
 import org.omg.CORBA.*;
-import org.omg.CORBA.ORBPackage.InvalidName;
 import VS2.*;
 
-
 public class StartClient {
-	private String pWord;
-	private String uName;
+	UserData userData;
+	LoginInformation loginInfo;
+
 	private int uid = 12345;
 	private String message;
 	private String fatherName;
 	
-	public MessageData msg;
+	
 	private MessageboardServerInterface mbImpl;
+	private LoginServerInterface dbImpl;
 	boolean shutdown;
-	
-	
-	ArrayList<MessageData> messageList;
-	
+	boolean angemeldet = true;//wenn db da ist auf false setzten
+	private ArrayList<MessageData> messageList;
+	private ArrayList<String> childList;
+
 	ORB orb;
-	private String[] url ;
-	private String METHOD = "DataServiceName1"; 
-	private void connectToServer(){
+	private String[] url=null;
+	private String METHOD = "DataServiceName1";
+
+	private void connectToServer() {
 		try {
 			// Initialisiere ORB und beschaffe Zugang zum 'NameService'
-		
-		// create and initialize the ORB
+
+			// create and initialize the ORB
 			this.orb = ORB.init(url, null);
 
 			// get the root naming context
 			org.omg.CORBA.Object objRef;
-
 			objRef = orb.resolve_initial_references("NameService");
-			
+
 			// Use NamingContextExt instead of NamingContext. This is
 			// part of the Interoperable naming Service.
 			NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
-
 			mbImpl = MessageboardServerInterfaceHelper.narrow(ncRef.resolve_str(this.METHOD));
-			
-			
-			
+//			dbImpl = LoginServerInterfaceHelper.narrow(ncRef.resolve_str(this.METHOD));
+
 		} catch (Exception e) {
 			System.out.println("ERROR : " + e);
 			System.exit(0);
 		}
 	}
-	public StartClient(String ip,int port) {
-		System.out.println(ip+port);
-		this.messageList = new ArrayList<MessageData>();
-		this.url = new String[] { "-ORBInitialPort", Integer.toString(port), "-ORBInitialHost", ip };
-		this.connectToServer();
-	}
-	public StartClient(String[] url) {
+	public StartClient(String uName, String pWord){
+		if(loginBenutzer(uName,pWord)){
+			this.messageList = new ArrayList<MessageData>();
+			this.childList =new ArrayList<String>();
+			this.url = new String[] { "-ORBInitialPort", Integer.toString(this.loginInfo.server.port), "-ORBInitialHost", this.loginInfo.server.ip };
+			this.connectToServer();
+		}
 		
-		this.messageList = new ArrayList<MessageData>();
-		//this.url = new String[] { "-ORBInitialPort", Integer.toString(port), "-ORBInitialHost", ip };
-		this.url=url;
-		this.connectToServer();
 	}
-	
 	/*
-	 * MessageBoard hat fatherId-> abruf xml datei auf fathername
+	 * 
+	 * TODO warte auf DB dann loeschen
 	 */
-	public String getFatherName(){
-		return this.mbImpl.getFatherName();	
+	public StartClient(String ip, int port) {
+		
+			this.messageList = new ArrayList<MessageData>();
+			this.childList =new ArrayList<String>();
+			this.url = new String[] { "-ORBInitialPort", Integer.toString(port), "-ORBInitialHost", ip };
+			//this.url = new String[] { "-ORBInitialPort", Integer.toString(this.loginInfo.server.port), "-ORBInitialHost", this.loginInfo.server.ip };
+			this.connectToServer();
+		 
+
 	}
-	
 	/*
-	 *abrufen von den KindsServer Name
+	 * 
 	 */
-	public String[] getChildNames(){
-		return this.mbImpl.getChildNames();
+	public  StartClient(String[] url) {
+	
+			this.messageList = new ArrayList<MessageData>();
+			// this.url = new String[] { "-ORBInitialPort",
+			// Integer.toString(port),
+			// "-ORBInitialHost", ip };
+			this.url = url;
+			this.connectToServer();
+		
+	}
+
+	/*
+	 * login wenn werte leer sind kommt false
+	 */
+
+	public boolean loginBenutzer(String uName, String pWord) {
+		if (uName.isEmpty() || pWord.isEmpty()) {
+			angemeldet = false;
+			return false;
+		} else {
+			this.userData =new UserData(uName,pWord);
+			loginDB();
+			angemeldet = true;
+			return true;
+		}
+	}
+
+	/*
+	 * TODO Datenbank implimentieren
+	 */
+	private void loginDB() {
+//		this.loginInfo =new LoginInformation(this.dbImpl.login(userData).adminRights,this.dbImpl.login(userData).server);
+		
+	}
+
+	/*
+	 * abrufen von den KindsServer Name
+	 */
+	public ArrayList<String> getChildNames() {
+		childList.clear();
+		String[] tempArrayChilds = mbImpl.getChildNames();
+		if (!tempArrayChilds .equals(null)) {
+			for (int y = 0; y < tempArrayChilds .length; y++) {
+				childList.add(tempArrayChilds [y]);
+			}
+		}
+		return childList;	
 	}
 	
+	public String getChildIP(String childname){
+		return this.mbImpl.connectToChild(childname).ip;
+	}
+	
+	public int getChildPort(String childname){
+		return this.mbImpl.connectToChild(childname).port;
+	}
 	/*
-	 * Array von alle Nachrichten 
+	 * Array von alle Nachrichten
 	 */
 	public ArrayList<MessageData> getMessage() {
 		messageList.clear();
 		MessageData[] tempArray = mbImpl.getMessages();
-		if(!tempArray.equals(null)){
+		if (!tempArray.equals(null)) {
 			for (int y = 0; y < tempArray.length; y++) {
-			messageList.add(tempArray[y]);
+				messageList.add(tempArray[y]);
 			}
 		}
 		return messageList;
 	}
 
 	/*
-	 * ersetzt die Nachricht 
+	 * ersetzt die Nachricht
 	 */
-	public boolean setMessage(String newmessage,String messageID, int uid, String uName) {
-
-		return this.mbImpl.setMessage(message, messageID, uid,uName);
-
+	public boolean setMessage(String newmessage, String messageID, int uid, String uName) {
+		
+		return this.mbImpl.setMessage(message, messageID, uid, uName);
 	}
 
 	/*
 	 * loescht die Nachricht
+	 * 
 	 * @param uid
-	 * @param messageID 
+	 * 
+	 * @param messageID
 	 */
 	public boolean deleteMessage(int uid, String messageID) {
 
 		return this.mbImpl.deleteMessage(uid, messageID);
 	}
-	
+
 	/*
 	 * schreib eine neue Nachricht
+	 * TODO
 	 */
 	public boolean schreibeMessage(String message) {
-		return mbImpl.createNewMessage(message, this.uid, this.uName);
+		return mbImpl.createNewMessage(message, this.uid, this.userData.userName);
 	}
-	
+
 	/*
 	 * gibt die direkt nächste Message
+	 * TODO
 	 */
 	public MessageData getnextMessage() {
-		
 		return this.mbImpl.getPreviousMessage();
 	}
-	
+
 	/*
 	 * gibt die vorhärige Message
 	 */
@@ -140,45 +191,62 @@ public class StartClient {
 	}
 
 	/*
-	 * um mit dem Vater zu verbinden 
-	 * Benutzte erst getFatherIp ud Port
-	 * und dann start()
+	 * um mit dem Vater zu verbinden Benutzte erst getFatherIp ud Port und dann
+	 * start()
 	 */
-	public String getFatherIP(){
+	public String getFatherIP() {
 		return mbImpl.connectToFather().ip;
 	}
-	public int getFatherPort(){
+
+	public int getFatherPort() {
 		return mbImpl.connectToFather().port;
 	}
-	/*
-	 * username schreiben 
-	 */
-	public void writeUser(String user) {
-		this.uName = user;
-	}
 	
-	public void writePassword(String pword) {
-		this.pWord = pword;
+	/*
+	 * MessageBoard hat fatherId-> abruf xml datei auf fathername
+	 */
+	public String getFatherName() {
+		return this.mbImpl.getFatherName();
 	}
 
+	/*
+	 *@ Para MessageData
+	 *Sendet Nachircht an alle Kinder
+	 *Return boolean
+	 */
+	public boolean iterateChilds(MessageData tempMData){
+		return this.mbImpl.iterateChilds(tempMData.text,tempMData.id,tempMData.uid,tempMData.uName,this.loginInfo.adminRights);
+	}
+	
+	/*
+	 *@ Para MessageData
+	 *Sendet Nachircht an Vater
+	 *Return boolean 
+	 */
+	public boolean  publishOnFather(MessageData tempMData){
+		return this.mbImpl.publishOnFather(tempMData.text, tempMData.id, tempMData.uid, tempMData.uName);
+	}
+	/*
+	 * username abfragen
+	 */
 	public String getUser() {
-		return this.uName;
+		return this.userData.userName;
 	}
 
 	public int getUserID() {
+		return generateID();
+	}
+
+	private int generateID() {
 		return this.uid;
+
 	}
-	
-	private void generateID(){
-		
-	}
+
 	/*
 	 * disconnect from server
 	 */
-	public void disconnectToServer(){
-		
-			orb.destroy();
-		
+	public void disconnectToServer() {
+		orb.destroy();
 	}
-	
+
 }
