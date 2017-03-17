67d2f951a34bff3a3961857802f72aed97000920
Marco Palumbo <marco.palumbo-1991@web.de>
2017-03-17 10:06:38 +0100

    StartXXXX.sh Dateien in Hauptordner gelegt

--- StartClient.sh
+++ StartClient.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Benoetigt zum Starten zwei werte: IP des ORB Servers und Port 
-java Dialog -ORBInitialHost $1 -ORBInitialPort $2
+java ./client/Dialog -ORBInitialHost $1 -ORBInitialPort $2
--- server/StartServer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#Benoetigt zum Starten zwei werte: IP des ORB Servers und Port
-./StartServer -ORBInitRef NameService=corbaloc::$1:$2/NameService -ORBtraceLevel 5
