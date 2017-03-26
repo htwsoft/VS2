04b5c2b35bd91182593b66a561e0261d4b5d848f
Marco Palumbo <marco.palumbo-1991@web.de>
2017-03-26 11:52:42 +0200

    Unit zum Testen des SoapServerClients hinzugefuegt

---
+++ server/SoapServerClientTest.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "./src/SoapServerClient.h"
+
+int main(int argc, char ** args)
+{
+    SoapServerClient  * ssc = 0;
+    cout << "----------------" << endl;
+    cout << "Soap-Client-Test" << endl;
+    cout << "----------------" << endl;
+    ssc = new SoapServerClient(1000, "http://localhost:8000");
+    cout << "Input: " << "14-1002" << endl;
+    ssc->sendMessage(4, 14, "Test String", "14-1002", 12345);
+    cout << "----------------" << endl;
+    cout << "Input: " << "15-1002" << endl;
+    ssc->sendMessage(4, 14, "Test String", "15-1002", 12345);
+    cout << "----------------" << endl;
+    cout << "Input: " << "4-SOAP412" << endl;
+    ssc->sendMessage(4, 14, "Test String", "4-SOAP412", 12345);
+    cout << "----------------" << endl;
+    cout << "Input: " << "-5-SOAP1002" << endl;
+    ssc->sendMessage(4, 14, "Test String", "-5-SOAP1002", 12345);
+    cout << "----------------" << endl;
+    delete ssc;
+    cout << "Ende-Test" << endl;
+    cout << "----------------" << endl;
+    return 0;
+}
